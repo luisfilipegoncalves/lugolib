dd7e113052085ba0b68edc1fd8d9e99b7ab45e23
Luis Gonçalves <exnlesi@gmail.com>
2020-12-06 20:44:47 +0000

    add avatar default export

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 
+export { default as Avatar } from "./atoms/avatar";
+
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
   children?: ReactChild;
